f64c1cac872ffa4c302dc3e8c986c500b7e865c5
paundraP <paundrapujodarmawan@gmail.com>
2025-06-05 03:29:35 +0700

    feat(student): implementation for adding course to student

---
+++ src/student.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <student.h>
+using namespace std;
+
+// the implementation of adding the course to the student
+void Student::addCourse(string course) {
+    // checking whether the course is already in the student course list using for each.
+    for(const auto &c: courses) {
+        // if the course is in the courses list, thats mean that the course the user want to add is already on the list
+        if(c.first == course) {
+            cout << "Course" << course << " already added" << endl;
+            return;
+        }
+    }
+
+    // Default course grade is 0 that means the course is ongoing
+    courses.push_back(make_pair(course, 0));
+    cout << "Course " << course << " has been added." << endl;
+}
